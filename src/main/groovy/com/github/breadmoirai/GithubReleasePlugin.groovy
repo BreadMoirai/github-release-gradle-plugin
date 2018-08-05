13c53af56c57a12755d8772e4d272952c3ce2cdf
BreadMoirai <dreadmoirai@gmail.com>
2018-08-05 00:55:03 -0700

    Set default values after build script evaluation and before running task. All properties are now optional as long as project name, group, and version is defined.

--- src/main/groovy/com/github/breadmoirai/GithubReleasePlugin.groovy
+++ src/main/groovy/com/github/breadmoirai/GithubReleasePlugin.groovy
@@ -18,6 +18,7 @@ package com.github.breadmoirai
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.provider.Property
 
 class GithubReleasePlugin implements Plugin<Project> {
 
@@ -39,5 +40,30 @@ class GithubReleasePlugin implements Plugin<Project> {
                 setReleaseAssets ext.releaseAssets
             }
         }
+
+        project.afterEvaluate {
+            def self = project.plugins.findPlugin(GithubReleasePlugin)
+            if (self) {
+                GithubReleaseExtension e = project.extensions.getByType(GithubReleaseExtension)
+                setOrElse(e.owner) {
+                    def group = project.group.toString()
+                    group.substring(group.lastIndexOf('.') + 1)
+                }
+                setOrElse(e.repo) {
+                    project.name ?: project.rootProject?.name ?: project.rootProject?.rootProject?.name
+                }
+                setOrElse(e.tagName) { "v${project.version}" }
+                setOrElse(e.targetCommitish) { 'master' }
+                setOrElse(e.releaseName) { "${project.version}" }
+                setOrElse(e.body) { "" }
+                setOrElse(e.draft) { false }
+                setOrElse(e.prerelease) { false }
+                setOrElse(e.token) { "" }
+            }
+        }
+    }
+
+    private static <P, V extends P> void setOrElse(Property<P> prop, Closure<V> value) {
+        if (!prop.isPresent()) prop.set(value())
     }
 }
